64e5587cb9af5de9f731c1e48ae7c1efa0ad8c20
Linus Bernhard <libf24@student.bth.se>
2026-09-24 11:29:10 +0200

    added app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "ThreMoLIA Game"
+    return render_template("DFD_working_example.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
